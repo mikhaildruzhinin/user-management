6616e2a50bc0b0eaa025dea02edc1dc2cc9554db
Mikhail Druzhinin <druzhininmy@gmail.com>
2025-01-12 17:22:43 +0300

    update EndpointsSpec

--- src/test/scala/ru/mikhaildruzhinin/usermanagement/EndpointsSpec.scala
+++ src/test/scala/ru/mikhaildruzhinin/usermanagement/EndpointsSpec.scala
@@ -1,55 +1,70 @@
 package ru.mikhaildruzhinin.usermanagement
 
-import ru.mikhaildruzhinin.usermanagement.Endpoints._
+import io.circe.Decoder
+import io.circe.generic.auto._
 import org.scalatest.EitherValues
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
+import ru.mikhaildruzhinin.usermanagement.Endpoints._
+import ru.mikhaildruzhinin.usermanagement.Library._
+import sttp.capabilities.WebSockets
+import sttp.client3._
+import sttp.client3.circe._
 import sttp.client3.testing.SttpBackendStub
-import sttp.client3.{UriContext, basicRequest}
+import sttp.model.Method
+import sttp.tapir.server._
 import sttp.tapir.server.stub.TapirStubInterpreter
 
-import Library._
-import io.circe.generic.auto._
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.concurrent.duration.Duration
-import sttp.client3.circe._
 
-class EndpointsSpec extends AnyFlatSpec with Matchers with EitherValues {
+class EndpointsSpec extends AsyncFlatSpec with Matchers with EitherValues {
+
+  private val baseUriStub = "http://test.com"
+
+  private val backendStub = (endpoint: ServerEndpoint[Any, Future]) => {
+    TapirStubInterpreter(SttpBackendStub.asynchronousFuture)
+      .whenServerEndpointRunLogic(endpoint)
+      .backend()
+  }
+
+  // @formatter:off
+  private def sendRequest[A](method: Method, uri: String,
+                             backend: SttpBackend[Future, WebSockets])
+                            (implicit decoder: Decoder[A]) = {
+
+    basicRequest
+      .method(method, uri"$baseUriStub/${uri.split("/").toSeq}")
+      .response(asJson[A])
+      .send(backend)
+  }
+  // @formatter:on
 
   it should "return hello message" in {
+    val endpoint = helloServerEndpoint
     // given
-    val backendStub = TapirStubInterpreter(SttpBackendStub.asynchronousFuture)
-      .whenServerEndpointRunLogic(helloServerEndpoint)
-      .backend()
+    val backend: SttpBackend[Future, WebSockets] = backendStub(endpoint)
 
     // when
     val response = basicRequest
-      .get(uri"http://test.com/hello?name=adam")
-      .send(backendStub)
+      .get(uri"$baseUriStub/hello?name=adam")
+      .send(backend)
 
     // then
-    response.map(_.body.value shouldBe "Hello adam").unwrap
+    response.map(_.body.value shouldBe "Hello adam")
   }
 
   it should "list available books" in {
     // given
-    val backendStub = TapirStubInterpreter(SttpBackendStub.asynchronousFuture)
-      .whenServerEndpointRunLogic(booksListingServerEndpoint)
-      .backend()
+    val endpoint = booksListingServerEndpoint
 
     // when
-    val response = basicRequest
-      .get(uri"http://test.com/books/list/all")
-      .response(asJson[List[Book]])
-      .send(backendStub)
+    val response = sendRequest[Seq[Book]](
+      method = Method.GET,
+      uri = "books/list/all",
+      backend = backendStub(endpoint)
+    )
 
     // then
-    response.map(_.body.value shouldBe books).unwrap
-  }
-
-  implicit class Unwrapper[T](t: Future[T]) {
-    def unwrap: T = Await.result(t, Duration.Inf)
+    response.map(_.body.value shouldBe books)
   }
 }
