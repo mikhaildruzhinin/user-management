a2f36a7b43f9eea1adf55d6f9bfcce8c419b2abf
Mikhail Druzhinin <druzhininmy@gmail.com>
2025-01-22 23:07:39 +0300

    update EndpointsSpec

--- src/test/scala/ru/mikhaildruzhinin/usermanagement/EndpointsSpec.scala
+++ src/test/scala/ru/mikhaildruzhinin/usermanagement/EndpointsSpec.scala
@@ -11,7 +11,7 @@ import ru.mikhaildruzhinin.usermanagement.Endpoints._
 import sttp.client3._
 import sttp.client3.circe._
 import sttp.client3.testing.SttpBackendStub
-import sttp.model.Method
+import sttp.model._
 import sttp.tapir.server._
 import sttp.tapir.server.stub.TapirStubInterpreter
 
@@ -72,58 +72,48 @@ class EndpointsSpec extends AsyncFlatSpec with Matchers with EitherValues {
 
   it should "register" in {
 
-    // given
     implicit val endpoint: ServerEndpoint[Any, Future] = registerEndpoint
     val body = UserBody("admin", "admin")
 
-    // when
     val response = sendRequest[UserBody, UserDto](
       method = Method.POST,
       uri = "register",
       body = body
     )
 
-    // then
     response.map(_.body.value shouldBe UserDto("admin"))
   }
 
   it should "successful login" in {
 
-    // given
     implicit val endpoint: ServerEndpoint[Any, Future] = loginEndpoint
     val body = UserBody("admin", "admin")
 
-    // when
     val response = sendRequest[UserBody, AuthenticationToken](
       method = Method.POST,
       uri = "login",
       body = body
     )
 
-    // then
     response.map(_.body.value shouldBe an[AuthenticationToken])
   }
 
   it should "unsuccessful login" in {
 
-    // given
     implicit val endpoint: ServerEndpoint[Any, Future] = loginEndpoint
     val body = UserBody("not_admin", "not_admin")
 
-    // when
     lazy val response = sendRequest[UserBody, AuthenticationToken](
       method = Method.POST,
       uri = "login",
       body = body
     )
 
-    // then
-    response.map(x => x.body shouldBe a[Left[HttpError[String], AuthenticationToken]])
+    response.map(_.code shouldBe StatusCode.BadRequest)
   }
 
   it should "successful test" in {
 
-    // given
     val claim = JwtClaim(
       expiration = Some(Instant.now.plus(10L, ChronoUnit.MINUTES).getEpochSecond),
       issuedAt = Some(Instant.now.getEpochSecond),
@@ -134,7 +124,6 @@ class EndpointsSpec extends AsyncFlatSpec with Matchers with EitherValues {
     val token = AuthenticationToken(JwtCirce.encode(claim, key, algorithm))
     implicit val endpoint: ServerEndpoint[Any, Future] = testEndpoint
 
-    // when
     val response = sendRequest[Message](
       method = Method.GET,
       uri = "test",
@@ -144,5 +133,24 @@ class EndpointsSpec extends AsyncFlatSpec with Matchers with EitherValues {
     response.map(_.body.value shouldBe Message("ok"))
   }
 
-  // TODO: unsuccessful test
+  it should "unsuccessful test" in {
+
+    val claim = JwtClaim(
+      expiration = Some(Instant.now.plus(10L, ChronoUnit.MINUTES).getEpochSecond),
+      issuedAt = Some(Instant.now.getEpochSecond),
+      content = UserDto("admin").asJson.noSpaces
+    )
+    val key = "wrong_key"
+    val algorithm = JwtAlgorithm.HS256
+    val token = AuthenticationToken(JwtCirce.encode(claim, key, algorithm))
+    implicit val endpoint: ServerEndpoint[Any, Future] = testEndpoint
+
+    val response = sendRequest[Message](
+      method = Method.GET,
+      uri = "test",
+      token = token
+    )
+
+    response.map(_.code shouldBe StatusCode.BadRequest)
+  }
 }
